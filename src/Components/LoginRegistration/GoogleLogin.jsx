17679bb3025fa0a24ec9f901d1ef02d153bf1cbe
sayeda <hasinadilruba2021@gmail.com>
2024-05-28 15:29:40 +0200

    login with google done

--- src/Components/LoginRegistration/GoogleLogin.jsx
+++ src/Components/LoginRegistration/GoogleLogin.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const GoogleLogin = () => {
+   const { googleLogin } = useAuth();
+
+  const handleGoogleSignIn = () =>{
+     googleLogin()
+  };
+
  return (
   <div>
-     <button className='btn btn-outline bg-base-300 text-red-600 w-full'>CONTINUE WITH GOOGLE </button>
+     <button onClick={handleGoogleSignIn} className='btn btn-outline bg-base-300 text-red-600 w-full'>CONTINUE WITH GOOGLE </button>
   </div>
  );
 };
